bde318dc5375d5e4d4958fed0ef127fcd718665e
keepingrun <1048199278@qq.com>
2019-06-14 17:20:08 +0800

    增加服务，设置AlarmManager，实现后台自动更新天气缓存

--- app/src/main/java/com/example/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.coolweather.gson.Forecast;
 import com.example.coolweather.gson.Weather;
+import com.example.coolweather.service.AutoUpdateService;
 import com.example.coolweather.util.HttpUtil;
 import com.example.coolweather.util.Utility;
 
@@ -95,12 +97,14 @@ public class WeatherActivity extends AppCompatActivity {
             weatherId=weather.basic.weatherId;
             showWeatherInfo(weather);
         }else{
+            //根据跳转过来的intent的数据
             weatherId=getIntent().getStringExtra("weather_id");
             //请求数据时layout不可见
             weatherLayout.setVisibility(View.INVISIBLE);
             //根据weather_id请求数据
             requestWeather(weatherId);
         }
+        //根据weatherId再次获取天气数据
         swipe_refresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
            @Override
             public void onRefresh() {
@@ -179,6 +183,10 @@ public class WeatherActivity extends AppCompatActivity {
      *  处理显示Weather实体类中的数据
      */
     private void showWeatherInfo(Weather weather) {
+        if(weather!=null&&"ok".equals(weather.status)){
+            Intent intent=new Intent(this, AutoUpdateService.class);
+            startService(intent);
+        }
         //所选县
         titleCity.setText(weather.basic.cityname);
         //split返回的是数组
